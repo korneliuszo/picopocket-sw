02dbd1445842d669186f6110cf0482233962795f
Korneliusz Osmenda <korneliuszo@gmail.com>
2024-12-08 18:57:46 +0100

    Run at real hardware

--- main.cpp
+++ main.cpp
@@ -14,35 +14,23 @@
 #include "www.hpp"
 #include "config.hpp"
 #include <isa_worker.hpp>
+#include "or/or.hpp"
+#include <ioiface.hpp>
 
 constexpr uint32_t PICO_Freq=250; //PM_SYS_CLK;
 
-
-extern "C" const uint8_t _binary_optionrom_bin_start[];
-extern "C" const uint8_t _binary_optionrom_bin_end[];
-extern "C" const uint8_t _binary_optionrom_bin_size[];
-
-[[gnu::section(".core1_static")]]
-static uint32_t read_fn(void* obj, uint32_t faddr)
-{
-	volatile uint8_t * arr = static_cast<volatile uint8_t*>(obj);
-	uint8_t ret = arr[faddr];
-	return ret;
-}
-
-[[gnu::section(".core1_static")]]
-static void nop_wrfn(void* obj, uint32_t faddr, uint8_t data) {}
-
 static void copy_optionroms(void) {
-  extern char __core1_static_start__[];
-  extern char __core1_static_end__[];
-  extern char __core1_static_source__[];
+	extern char __core1_static_start__[];
+	extern char __core1_static_end__[];
+	extern char __core1_static_source__[];
 
-  memcpy(__core1_static_start__, __core1_static_source__, (__core1_static_end__-__core1_static_start__));
+	memcpy(__core1_static_start__, __core1_static_source__, (__core1_static_end__-__core1_static_start__));
 }
+
 
 int main(void)
 {
+	Thread main_thread;
 
 	// Overclock!
 	vreg_set_voltage(VREG_VOLTAGE_1_25);
@@ -53,23 +41,18 @@ int main(void)
 
 	Config::Flash_Storage::load();
 
+	copy_optionroms();
+	network_init();
+
+	optionrom_install(&main_thread);
+
+	IoIface::ioiface_install();
+
 	ISA_Init();
 
 	tusb_init();
 
-	network_init();
-
 	www_init();
-	copy_optionroms();
-	add_device({
-					.start = 0xC8000,
-					.size = (uint32_t)_binary_optionrom_bin_size,
-					.type = Device::Type::MEM,
-					.rdfn = read_fn,
-					.wrfn = nop_wrfn,
-					.obj = (void*)_binary_optionrom_bin_start,
-	});
-
 
 	bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_PROC1_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS | BUSCTRL_BUS_PRIORITY_DMA_W_BITS;
 	ISA_Start();
@@ -78,6 +61,8 @@ int main(void)
 	{
 		tud_task(); // tinyusb device task
 		network_poll();
+		optionrom_start_worker(&main_thread);
+		main_thread.yield();
 	}
 
 	return 0;
